b28d83bf40f5cf798f6cae2df7c959ac56f98ddd
JITENDRASANODIYA <jittu.sanodiya@gmail.com>
2021-08-08 19:25:09 +0530

    new change of print comitted

---
+++ src/main/scala/ScalaDemo.scala
@@ -0,0 +1,16 @@
+object ScalaDemo {
+  def main(args: Array[String]): Unit = {
+    println("My First scala program")
+
+    var a = 3
+    var b = 5
+    var c = a+b
+    println("a+b=" + c)
+
+    def printName(name:String)= println(s"My name is $name")
+
+    printName("abc")
+
+
+  }
+}
