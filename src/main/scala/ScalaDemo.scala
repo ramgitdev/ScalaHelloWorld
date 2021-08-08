3b46cf95256422f0bd23f3293c8217fc0af9e72c
JITENDRASANODIYA <jittu.sanodiya@gmail.com>
2021-08-08 19:25:09 +0530

    Initial commit

---
+++ src/main/scala/ScalaDemo.scala
@@ -0,0 +1,11 @@
+object ScalaDemo {
+  def main(args: Array[String]): Unit = {
+    println("My First scala program")
+
+    var a = 3
+    var b = 5
+    var c = a+b
+    println("a+b=" + c)
+
+  }
+}
---
+++ target/classes/practice.sc
@@ -0,0 +1,4 @@
+println("Hello 123")
+var a =3
+var b =3
+a+b
